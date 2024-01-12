a9b654094f58598643ad988565fa9cdff481f049
lucanchling <luc.anchling@cpe.fr>
2024-01-12 16:56:21 +0100

    Interpolate ball positions for multiple frame numbers

--- tracking/interp_tracking.py
+++ tracking/interp_tracking.py
@@ -43,29 +43,62 @@ def main(args):
 
     positions = get_positions(videoName, yellowLower, yellowUpper)
 
-    N = 20 # number of frame (before and after) to interpolate
+    # N = 20 # number of frame (before and after) to interpolate
+    N_list = [2, 5, 10]
+    skip = 1
+    colors = [(0, 255, 0), (255, 0, 0), (0, 0, 255), (255, 128, 0), (56, 140, 128), (128, 0, 255), (255, 0, 128), (128, 255, 0), (0, 128, 255)]
+    center_list = []
+    for N in N_list:
+        positions_temp = positions.copy()
+        centers = []
+        for i_frame in range(len(positions)):
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
+            # _, frame = cap.read()
+            center = positions_temp[i_frame]
+            before, after = [], []
+            if center is None:
+                # print(f"Ball not detected at frame {i_frame}")
+                # interpolate the missing center
+                
+                # before = [center for center in positions_temp[i_frame-N:i_frame] if center is not None]
+                for i in range(i_frame-N, i_frame):
+                    if positions_temp[i] is not None:
+                        before.append([i_frame-i, positions_temp[i]])
 
-    for i_frame in range(N, len(positions)-N):
+                # after = [center for center in positions_temp[i_frame+1:i_frame+N+1] if center is not None]
+                for i in range(i_frame+1, i_frame+N+1):
+                    if positions_temp[i] is not None:
+                        after.append([i-i_frame, positions_temp[i]])
+                
+                if len(before) == 0 or len(after) == 0:
+                    center = None
+
+                else:
+                    center = (int(np.mean([c[1][0] for c in before + after])), 
+                              int(np.mean([c[1][1] for c in before + after])))
+                    # pass
+
+            # update the center
+            positions_temp[i_frame] = center
+
+            centers.append(center)
+        center_list.append(centers)
+
+    for i_frame in range(len(positions)):
         cap.set(cv2.CAP_PROP_POS_FRAMES, i_frame)
         _, frame = cap.read()
-        center = positions[i_frame]
 
-        if center is None:
-            # print(f"Ball not detected at frame {i_frame}")
-            # interpolate the missing center
-            before = [center for center in positions[i_frame-N:i_frame] if center is not None]
-            after = [center for center in positions[i_frame+1:i_frame+N+1] if center is not None]
-            if len(before) == 0 or len(after) == 0:
-                continue
-            center = (int(np.mean([c[0] for c in before + after])), int(np.mean([c[1] for c in before + after])))
-            
-        cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        for i in range(len(N_list)):
+            N = N_list[i]
+            cv2.circle(frame, center_list[i][i_frame], 5, colors[i], -1)
+            cv2.putText(frame, f"N={N}", (10, 30 + 30*i), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors[i], 2)
+
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
-        
+    
         # wait the time between two frames
         time.sleep(1/cap.get(cv2.CAP_PROP_FPS))
-
+        # time.sleep(0.2)
         if key == ord("q"):
             break
 
@@ -75,7 +108,7 @@ def main(args):
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("-v", "--video", type=str,
-        help="path to input video file", default="Video/test2.avi")
+        help="path to input video file", default="Video/test1.avi")
     args = vars(args.parse_args())
 
     main(args)
