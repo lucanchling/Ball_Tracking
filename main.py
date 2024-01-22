92d5a9490ae391d04745ec82c8b983e8f0fd3ac9
Teklaw7 <timothee.teyssier@cpe.fr>
2024-01-22 15:50:32 +0100

    plus qu'a tester

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from tracking.ball_tracking import get_ball_position
 from tracking import box_tracking
 from reco3D.epip_lines import get_corners
 import matplotlib.pyplot as plt
+import numpy as np
+from reco3D.minimize_dist import get_optimal_points
 
 
 ### Calibrage des caméras
@@ -37,6 +39,8 @@ if __name__ == "__main__":
     ret, M1, d1, M2, d2, R, T, E, F = cv2.stereoCalibrate(objp, corners_cam1, corners_cam2, 
                         Mint, dist, Mint, dist, 
                         (633, 470), flags=cv2.CALIB_FIX_INTRINSIC)
+    corners_cam1 = corners_cam1[0].reshape(88,2)
+    corners_cam2 = corners_cam2[0].reshape(88,2)
 
     cap1 = cv2.VideoCapture(id_cam1)
     cap2 = cv2.VideoCapture(id_cam2)
@@ -53,10 +57,74 @@ if __name__ == "__main__":
         print("ball_detected_2 : ", ball_detected_2)
         print("center_1 : ", center_1)
         print("center_2 : ", center_2)
-        
+        center_1 = (200,120)
+        center_2 = (240,100)
+        print("center1 : ", center_1)
+        print("center2 : ", center_2)
+
+        t_vec_L, t_vec_R = Mext1[:,3], Mext2[:,3]
+
+        x = np.array([corners_cam1[1][0], corners_cam1[1][1], 1])
+        # x = np.array([corners_cam1[]])
+
+        x_prime = np.array([corners_cam2[1][0], corners_cam2[1][1], 1])
+        centerL, centerR = get_optimal_points(F, center_1, center_2)
+        center1 = np.asarray(center_1)
+        center2 = np.asarray(center_2)
+
+        # camera_center
+        C1 = np.array([0,0,0])
+        C2 = np.array([0,0,0])
+        # express C2 in C1 coordinate system
+        C2 = -R.transpose() @ T
+
+        C2 = C2.reshape(3,)
 
 
+        # project points to the camera plane
+        x_cam = np.linalg.inv(Mint) @ x # C1 plane
+        x_prime_cam = np.linalg.inv(Mint) @ x_prime # C2 plane
+        x_prime_cam = np.squeeze(x_prime_cam) 
+        # express C2 projected point in C1 plane
+        x_prime_cam = R.transpose() @ (x_prime_cam.reshape(3,1) - T)
+        x_prime_cam = x_prime_cam.reshape(3,)
+        # line from camera center towards the point
+        l1 = x_cam
+        l2 = x_prime_cam - C2
 
+
+        # normalize the lines
+        l1 = l1 / np.linalg.norm(l1)
+        l2 = l2 / np.linalg.norm(l2)
+
+        # get the 3D point with the shortest distance to both lines
+        n = np.cross(l1, l2)
+        n1 = np.cross(l1, n)
+        n2 = np.cross(l2, n)
+        # n = n / np.linalg.norm(n)
+    
+        c1 = C1 + (np.dot(C2- C1, n2))/np.dot(l1, n2) * l1
+        c2 = C2 + (np.dot(C1- C2, n1))/np.dot(l2, n1) * l2
+    
+        X = (c1 + c2) / 2
+    
+        print('3D point: ', X)
+
+        # plot the lines
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(0, 0, 0, c='b', marker='o')
+        ax.scatter(C2[0], C2[1], C2[2], c='r', marker='o')
+        # ax.scatter(x_prime_cam[0], x_prime_cam[1], x_prime_cam[2], c='g', marker='o')
+        # ax.scatter(c1[0], c1[1], c1[2], c='y', marker='o')
+        # ax.scatter(c2[0], c2[1], c2[2], c='c', marker='o')
+        ax.quiver(0, 0, 0, l1[0], l1[1], l1[2], length=2000, normalize=True, color='b')
+        ax.quiver(C2[0], C2[1], C2[2], l2[0], l2[1], l2[2], length=2000, normalize=True, color='r')
+        ax.scatter(X[0], X[1], X[2], c='c', marker='o')
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        plt.show()
 
 
     cap1.release()
