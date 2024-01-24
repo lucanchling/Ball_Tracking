e9e81d36dc9d1704448382d505ef33d7c18207f1
Teklaw7 <timothee.teyssier@cpe.fr>
2024-01-24 17:31:08 +0100

    pour luc

--- trajectoire.py
+++ trajectoire.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 PTS_to_SAVE = np.load('PTS_to_SAVE.npy')
 print(len(PTS_to_SAVE))
@@ -18,5 +19,41 @@ ax.scatter(COINS[:,0],COINS[:,1],COINS[:,2], c='r')
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
+
+
+
+PTS_to_SAVE_LUC = np.load('PTS_to_SAVE_LUC.npy')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(PTS_to_SAVE_LUC[:,0],PTS_to_SAVE_LUC[:,1],PTS_to_SAVE_LUC[:,2])
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+### create an animation by plotting one point at the time
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+L = []
+delta = 100
+def animate(i):
+    abc = ax.scatter(PTS_to_SAVE_LUC[i,0],PTS_to_SAVE_LUC[i,1],PTS_to_SAVE_LUC[i,2])
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    L.append(abc)
+    if i == delta:
+        L[0].remove()
+    if i > delta:
+        ### remove points
+        L[i-delta].remove()
+    return ax
+
+### réduire le nombre de points à afficher
+ani = animation.FuncAnimation(fig, animate, frames=len(PTS_to_SAVE_LUC), interval=10e-10, blit=False, repeat=False)
+ax.axes.set_xlim3d(left=-50, right=250)
+ax.axes.set_ylim3d(bottom=-200, top=150)
+ax.axes.set_zlim3d(bottom=400, top=1200)
+
+print(len(PTS_to_SAVE_LUC))
 
 plt.show()
